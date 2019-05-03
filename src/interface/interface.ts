5421274d13c7a940386788cb73698834a579555d
zhouzheng0646 <zhouzheng0646@qq.com>
2019-05-04 02:04:09 +0800

    类.笔记

---
+++ src/interface/interface.ts
@@ -0,0 +1,10 @@
+interface LabelledValue {
+    label: string;
+}
+
+function printLabel(labelledObj: LabelledValue) {
+    console.log(labelledObj.label);
+}
+
+let myObj = { label: "Size 10 Object" };
+printLabel(myObj);
